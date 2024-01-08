71653299642e020934e66abeac140e5c3e531312
Mat Taylor <mat.taylor@gmail.com>
2024-01-08 13:31:50 -0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/vibal/Notion-DoIt-e82121d47946414ea29ea0033277fcec?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
